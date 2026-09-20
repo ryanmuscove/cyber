0ed0cf729906b59d127a6550d5905869168f9f9b
Ryanmuscove <Ryanmuscove@gmail.com>
2026-09-19 23:43:13 -0500

    adding more code

--- Firewalls/firewall.py
+++ Firewalls/firewall.py
@@ -1,10 +1,16 @@
 import random
 
-def generate_random():
+def generate_random_ip():
     return f"192.168.1.{random.randint(0, 20)}"
 
+def check_firewall_rules(ip, rules):
+    for rule_ip, action in rules.items():
+        if ip == rule_ip:
+            return action
+    return "allow"
+
 def main():
-    firwall_rules = {
+    firewall_rules = {
         "192.168.1.1": "block",
         "192.168.1.4": "block",
         "192.168.1.9": "block",
@@ -13,10 +19,10 @@ def main():
         "192.168.1.19": "block",
     }
 #for loop running 12 times for network traffic simulation
-for _ in range(12):
-    ip_address = generate_random_ip()
-    action = check_firewall_rules(ip_address, firewall_rules)
-    random_number = random.randint(0, 9999)
-    print(f"IP: {ip_address}, Action: {action}, Random: {random_number}.")
+    for _ in range(12):
+            ip_address = generate_random_ip()
+            action = check_firewall_rules(ip_address, firewall_rules)
+            random_number = random.randint(0, 9999)
+            print(f"IP: {ip_address}, Action: {action}, Random: {random_number}.")
 if __name__ == "__main__":
    main()
