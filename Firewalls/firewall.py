e5472bb6f66aeb9b5cd6a6c855b46faf04f9508a
Ryanmuscove <Ryanmuscove@gmail.com>
2026-09-19 23:34:10 -0500

    adding code

--- Firewalls/firewall.py
+++ Firewalls/firewall.py
@@ -1,2 +1,22 @@
 import random
 
+def generate_random():
+    return f"192.168.1.{random.randint(0, 20)}"
+
+def main():
+    firwall_rules = {
+        "192.168.1.1": "block",
+        "192.168.1.4": "block",
+        "192.168.1.9": "block",
+        "192.168.1.13": "block",
+        "192.168.1.16": "block",
+        "192.168.1.19": "block",
+    }
+#for loop running 12 times for network traffic simulation
+for _ in range(12):
+    ip_address = generate_random_ip()
+    action = check_firewall_rules(ip_address, firewall_rules)
+    random_number = random.randint(0, 9999)
+    print(f"IP: {ip_address}, Action: {action}, Random: {random_number}.")
+if __name__ == "__main__":
+   main()
