ed7d25136bf844e330af9d8a8692a80ec23f41b3
Ryanmuscove <ryanmuscove@gmail.com>
2026-09-21 21:05:52 -0500

    adding more

--- Firewalls/DoS_Blocker.py
+++ Firewalls/DoS_Blocker.py
@@ -5,5 +5,19 @@ import time
 from collections import defaultdict
 from scapy.all import sniff, IP
 
+#limit of compariong traffic
 THRESHOLD = 40
 print(f"THRESHOLD: {THRESHOLD}")
+
+def packet_callback(packet):
+    src_ip = packet[IP].src
+    packet_count[src_ip] += 1
+    current_time = time.time()
+    time_interval = current_time - start_time[0]
+
+    if time_interval >= 1:
+        for ip, count in packet_count.items():
+            packet_rate = count / interval_time
+
+            if packet_rate > THRESHOLD and ip not in blocked_ips:
+                
